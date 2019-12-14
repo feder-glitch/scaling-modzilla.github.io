bd06019d4d1e34c9ce7e242cb0c84c2aa3b4fcb8
feder-glitch <58868586+feder-glitch@users.noreply.github.com>
2019-12-14 16:15:34 +0100

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,12 @@
+!DOCTYPE html>  -Dokumenttyp 
+<html>          umhüllt ganzen Inhalt (root-Element)
+  <head>        nicht auf der Seite aufzufinden (Schlüsselwörter, Seitenbeschreibung)
+    <meta charset="utf-8">  Zeichenkodierung
+    <title>Meine Testseite</title> Titel Der Seite
+  </head>
+  <body>        Was die Betrachter sehen (Text, Bilder,Videos, usw)
+
+  </body>
+    <img src="images/firefox-icon.png" alt="Das Firefox-Logo: Ein brennender Fuchs, welcher die Erde umkreist">
+  </body>
+</html>
